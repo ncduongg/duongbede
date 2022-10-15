03152c59a2730a1fc0c06ef355d47dbb7f615bf6
Vũ Quang Minh <jokerdarkknight@protonmail.com>
2022-10-15 19:37:57 +0700

    update

--- src/components/Nav/SearchBox/SearchBox.tsx
+++ src/components/Nav/SearchBox/SearchBox.tsx
@@ -1,9 +1,12 @@
 import { component$, useStylesScoped$, $ } from "@builder.io/qwik";
+import { useLocation } from "@builder.io/qwik-city";
 import styles from './search-box.sass'
 import SearchIcon from "./SearchIcon";
 
 export default component$(() => {
     useStylesScoped$(styles)
+
+    const route = useLocation()
 
     const toggleMobileSearch = $((e: Event) => {
         if (window.innerWidth < 640) {
@@ -19,12 +22,19 @@ export default component$(() => {
         }
     })
 
-    return (
-        <>
-            <div id="search-box" onClick$={toggleMobileSearch} onMouseLeave$={closeMobileSearch}>
-                <SearchIcon />
-                <input type="search" id="search-input" placeholder="Type something to search...."/>
-            </div>
-        </>
-    )
+    const search = $((e: KeyboardEvent) => {
+        const el = e.target as HTMLInputElement
+        const value = el.value
+        const keycap = e.code as String
+        if (keycap === "Enter")  window.location.replace(`/search/${value}`)
+})
+
+return (
+    <>
+        <div id="search-box" onClick$={toggleMobileSearch} onMouseLeave$={closeMobileSearch}>
+            <SearchIcon />
+            <input onKeyUp$={search} type="search" id="search-input" placeholder="Type something to search...." />
+        </div>
+    </>
+)
 })
